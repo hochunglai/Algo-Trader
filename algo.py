2073811647712d11e26dc648f8c0df3609cd20bd
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-10-25 15:14:36 -0400

    With user input

--- algo.py
+++ algo.py
@@ -6,14 +6,16 @@ import datetime
 
 start = datetime.datetime(2016, 1, 1)
 end = datetime.datetime(2017, 1, 1)
-aapl = pdr.DataReader('AAPL', 'yahoo')
+
+ticker = input("Enter Ticker Symbol : ")
+stock = pdr.DataReader(ticker, 'yahoo')
 
 plt.style.use("dark_background")
 
-ma_50 = aapl['50-day'] = aapl['Adj Close'].rolling(50).mean()
-ma_100 = aapl['100-day'] = aapl['Adj Close'].rolling(100).mean()
+ma_50 = stock['50-day'] = stock['Adj Close'].rolling(50).mean()
+ma_100 = stock['100-day'] = stock['Adj Close'].rolling(100).mean()
 
-plt.plot(aapl['Adj Close'], label="Stock Price", color ="lightgray")
+plt.plot(stock['Adj Close'], label="Stock Price", color ="lightgray")
 plt.plot(ma_50, label="50 Days MA", color ="red")
 plt.plot(ma_100, label="100 Days MA", color ="purple")
 
