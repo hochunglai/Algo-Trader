93db1f6c26748df627c383b3f5adb4bbabf42631
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-10-26 09:50:25 -0400

    Live Data Interval 1 min

--- algo.py
+++ algo.py
@@ -3,44 +3,70 @@ import pandas as pd
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 import datetime
-
-start = datetime.datetime(2016, 1, 1)
-end = datetime.datetime.now()
-
-ticker = input("Enter Ticker Symbol : ")
-stock = pdr.DataReader(ticker, 'yahoo')
+import yfinance
 
 plt.style.use("dark_background")
 
-ma_50 = stock['50-day'] = stock['Adj Close'].rolling(50).mean()
-ma_100 = stock['100-day'] = stock['Adj Close'].rolling(100).mean()
+ticker = str(input("Enter Ticker Symbol : "))
+		
 
-buy_signals = []
-sell_signals = []
-trigger = 0
+def makeplot():
+	
+	start = datetime.datetime(2021, 10, 25)
+	end = datetime.datetime.now()
 
-for x in range(len(stock)):
-	if ma_50.iloc[x] > ma_100.iloc[x] and trigger != 1:
-		buy_signals.append(stock['Adj Close'].iloc[x])
-		sell_signals.append(float('nan'))
-		trigger = 1
-	elif  ma_50.iloc[x] < ma_100.iloc[x] and trigger != -1:
-		buy_signals.append(float('nan'))
-		sell_signals.append(stock['Adj Close'].iloc[x])
-		trigger = -1
-	else:
-		buy_signals.append(float('nan'))
-		sell_signals.append(float('nan'))
 
-print(stock)
+	stock = yfinance.download(tickers= ticker, period='1d', interval='1m')
+	print(stock)
+	
 
-stock['Buy Signals'] = buy_signals
-stock['Sell Signals'] = sell_signals
+	plt.plot(stock['Adj Close'], label="Stock Price", color ="lightgray", zorder=2)
 
-plt.plot(stock['Adj Close'], label="Stock Price", color ="lightgray", zorder=2)
-plt.plot(ma_50, label="50 Days MA", color ="#f7c325", linestyle = "--", zorder =1)
-plt.plot(ma_100, label="100 Days MA", color ="#ed7f1f", linestyle = "--", zorder =1)
-plt.scatter(stock.index, stock['Buy Signals'], label="Buy Signal", marker = "^",color ="#00ff00", lw=3, zorder=3)
-plt.scatter(stock.index, stock['Sell Signals'], label="Sell Signal", marker = "v",color ="#ff0000", lw=3, zorder=3)
-plt.legend(loc="upper left")
-plt.show()
+	plt.legend(loc="upper left")
+	plt.show()
+
+makeplot()
+
+
+"""
+u_ma1 = int(input("Enter First Moving Average : "))
+u_ma2 = int(input("Enter Second Moving Average : "))
+
+ma1 = stock[u_ma1,'-day'] = stock['Adj Close'].rolling(u_ma1).mean()
+	ma2 = stock[u_ma2,'-day'] = stock['Adj Close'].rolling(u_ma2).mean()
+
+	buy_signals = []
+	sell_signals = []
+	trigger = 0
+
+	for x in range(len(stock)):
+		if ma1.iloc[x] > ma2.iloc[x] and trigger != 1:
+			buy_signals.append(stock['Adj Close'].iloc[x])
+			sell_signals.append(float('nan'))
+			trigger = 1
+		elif  ma1.iloc[x] < ma2.iloc[x] and trigger != -1:
+			buy_signals.append(float('nan'))
+			sell_signals.append(stock['Adj Close'].iloc[x])
+			trigger = -1
+		else:
+			buy_signals.append(float('nan'))
+			sell_signals.append(float('nan'))
+
+	print(stock)
+
+	stock['Buy Signals'] = buy_signals
+	stock['Sell Signals'] = sell_signals
+
+	style = "--";
+
+	label_1 = str(u_ma1) + " Days MA"
+	label_2 = str(u_ma2) + " Days MA"
+
+
+		plt.plot(ma1, label=label_1, color ="#f7c325", linestyle = style, zorder =1)
+	plt.plot(ma2, label=label_2, color ="#ed7f1f", linestyle = "--", zorder =1)
+	plt.scatter(stock.index, stock['Buy Signals'], label="Buy Signal", marker = "^",color ="#00ff00", lw=3, zorder=3)
+	plt.scatter(stock.index, stock['Sell Signals'], label="Sell Signal", marker = "v",color ="#ff0000", lw=3, zorder=3)
+"""
+
+
